883052c78e648b2bb4ea70093f2a825227205138
iamrafehdev <abdulrafeh46@gmail.com>
2021-05-06 15:00:55 +0500

    Add Toogle + Delete Button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-function App() {
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from 'react';
+
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctor Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting in University',
+      day: 'Feb 6th at 1:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Shoppiing',
+      day: 'Feb 7th at 2:00pm',
+      reminder: false,
+    }
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((tasks) => tasks.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.filter((task) => tasks.id === id ?
+      { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className='container'>
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder} />) : ('No Tasks Yet!!!')}
     </div>
+
   );
 }
 
